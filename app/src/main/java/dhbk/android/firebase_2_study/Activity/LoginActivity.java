a11a93e9fba405493fb25d6468c5b300dcf6e346
Phong <p7157568@gmail.com>
2016-04-14 17:38:52 +0700

    feat - add register and login

--- app/src/main/java/dhbk/android/firebase_2_study/Activity/LoginActivity.java
+++ app/src/main/java/dhbk/android/firebase_2_study/Activity/LoginActivity.java
@@ -1,12 +1,27 @@
 package dhbk.android.firebase_2_study.Activity;
 
+import android.app.ProgressDialog;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.firebase.client.AuthData;
+import com.firebase.client.Firebase;
+import com.firebase.client.FirebaseError;
+
+import java.util.Map;
 
 import dhbk.android.firebase_2_study.Help.FirebaseConfig;
 import dhbk.android.firebase_2_study.R;
 
 public class LoginActivity extends AppCompatActivity {
+    private static final String TAG = LoginActivity.class.getName();
+    private Firebase mFirebaseReference;
+    private EditText mEditTextEmail;
+    private EditText mEitTextPassword;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -14,7 +29,59 @@ public class LoginActivity extends AppCompatActivity {
         setContentView(R.layout.activity_login);
 
         FirebaseConfig.getFirebaseInitialize(this);
+        mFirebaseReference = FirebaseConfig.getFirebaseReference();
 
-
+        initializeView();
     }
+
+    private void initializeView() {
+        // đăng ký
+        findViewById(R.id.button_register).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                final ProgressDialog progressDialog = ProgressDialog.show(LoginActivity.this, null, getString(R.string.register_progress_dialog), true);
+                mFirebaseReference.createUser(mEditTextEmail.getText().toString(), mEitTextPassword.getText().toString(),  new Firebase.ValueResultHandler<Map<String, Object>>() {
+                    @Override
+                    // dk thành công
+                    public void onSuccess(Map<String, Object> result) {
+                        progressDialog.dismiss();
+                        Toast.makeText(getApplicationContext(), "Thanh cong ", Toast.LENGTH_SHORT).show();
+                        Log.i(TAG, "Successfully created user account with uid: " + result.get("uid"));
+                    }
+
+                    @Override
+                    public void onError(FirebaseError firebaseError) {
+                        // there was an error
+                        progressDialog.dismiss();
+                        Log.i(TAG, "Error " + firebaseError.getMessage());
+                    }
+
+                });
+            }
+        });
+
+        // đăng nhập
+        findViewById(R.id.button_login).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                final ProgressDialog progressDialog = ProgressDialog.show(LoginActivity.this, " ", getString(R.string.login_progress_dialog), true);
+                mFirebaseReference.authWithPassword(mEditTextEmail.getText().toString(), mEitTextPassword.getText().toString(), new Firebase.AuthResultHandler() {
+                    @Override
+                    public void onAuthenticated(AuthData authData) {
+                        progressDialog.dismiss();
+                        Log.i(TAG, "Login success " + "User ID: " + authData.getUid() + ", Provider: " + authData.getProvider());
+                    }
+                    @Override
+                    public void onAuthenticationError(FirebaseError firebaseError) {
+                        // there was an error
+                        progressDialog.dismiss();
+                        Log.i(TAG, "Login failed! " +firebaseError.getMessage());
+                    }
+                });
+            }
+        });
+        mEditTextEmail = (EditText) findViewById(R.id.edit_txt_mail);
+        mEitTextPassword = (EditText) findViewById(R.id.edit_txt_pass);
+    }
+
 }
--- app/src/main/java/dhbk/android/firebase_2_study/Help/FirebaseConfig.java
+++ app/src/main/java/dhbk/android/firebase_2_study/Help/FirebaseConfig.java
@@ -8,7 +8,14 @@ import com.firebase.client.Firebase;
  * Created by huynhducthanhphong on 4/14/16.
  */
 public class FirebaseConfig {
+    private static final String FIREBASE_URL = "https://test-fb-phong093.firebaseio.com";
+    private static final String FIREBASE_CHILD = "chat";
+
     public static void getFirebaseInitialize(Context context) {
         Firebase.setAndroidContext(context);
+    }
+
+    public static Firebase getFirebaseReference() {
+        return new Firebase(FIREBASE_URL).child(FIREBASE_CHILD);
     }
 }
